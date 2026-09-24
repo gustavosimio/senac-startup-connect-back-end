72038d98b8b9f81abbc9dfd11cc9fb44572b06ae
julio cesar <juliocmcnha@gmail.com>
2026-09-23 23:32:45 -0300

    First Commit

---
+++ Controllers/FundingCallsController.cs
@@ -0,0 +1,66 @@
+﻿using aspnet_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_backend.Controllers;
+
+/// <summary>
+/// Provides endpoints for managing funding calls.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class FundingCallsController : ControllerBase
+{
+    /// <summary>
+    /// Retrieves all funding calls.
+    /// </summary>
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Retrieves a funding call by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the funding call.</param>
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Creates a new funding call.
+    /// </summary>
+    /// <param name="fundingCall">The funding call to be created.</param>
+    [HttpPost]
+    public IActionResult Create(FundingCall fundingCall)
+    {
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = fundingCall.Id },
+            fundingCall
+        );
+    }
+
+    /// <summary>
+    /// Updates an existing funding call.
+    /// </summary>
+    /// <param name="id">The unique identifier of the funding call.</param>
+    /// <param name="fundingCall">The updated funding call data.</param>
+    [HttpPut("{id:guid}")]
+    public IActionResult Update(Guid id, FundingCall fundingCall)
+    {
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Deletes a funding call.
+    /// </summary>
+    /// <param name="id">The unique identifier of the funding call.</param>
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        return NoContent();
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using aspnet_backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_backend.Controllers;
+
+/// <summary>
+/// Provides endpoints for managing users.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+
+    /// <summary>
+    /// Retrieves all users.
+    /// </summary>
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Retrieves a user by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the user.</param>
+    [HttpGet("{id:guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="user">The user to be created.</param>
+    [HttpPost]
+    public IActionResult Create(User user)
+    {
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = user.Id },
+            user
+        );
+    }
+
+    /// <summary>
+    /// Updates an existing user.
+    /// </summary>
+    /// <param name="id">The unique identifier of the user.</param>
+    /// <param name="user">The updated user data.</param>
+    [HttpPut("{id:guid}")]
+    public IActionResult Update(Guid id, User user)
+    {
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Deletes a user.
+    /// </summary>
+    /// <param name="id">The unique identifier of the user.</param>
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        return NoContent();
+    }
+}
